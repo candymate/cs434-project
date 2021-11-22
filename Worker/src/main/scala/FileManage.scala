c5055dc05f91fa40fc4acb301bdf91e82933dcfc
candymate <ys3f4@naver.com>
2021-11-22 11:39:04 -0800

    merge: multifile write code / test

--- Worker/src/main/scala/FileManage.scala
+++ Worker/src/main/scala/FileManage.scala
@@ -1,7 +1,6 @@
 import org.slf4j.LoggerFactory
 
-import java.io.File
-import java.io.RandomAccessFile
+import java.io._
 
 class Record(val key: String, val value: String) {
   override def toString: String = {
@@ -9,45 +8,36 @@ class Record(val key: String, val value: String) {
   }
 }
 
-class MultiFile(private[this] var fileList: List[File]) {
+class MultiFileRead(private[this] val fileList: List[File]) {
   val logger = LoggerFactory.getLogger(getClass)
 
-  private[this] var fileReadIdx, fileReadOff = 0
-  private[this] var fileWriteIdx, fileWriteOff = 0
-  private[this] var readOnly: Boolean = false
+  private[this] var fileIdx, fileOff = 0
 
-  def merge(mf1: MultiFile, mf2: MultiFile): MultiFile = {
-    mf1
-  }
-
-  def isEmpty(): Boolean = fileList.isEmpty
-
-  def isSingleFile(): Boolean = {
+  def length(): Int = {
     assert(fileList != Nil)
-    fileList.size == 1
+    fileList.foldLeft(0)((acc, f) => acc + f.length.toInt)
   }
 
   def seekRead(idx: Int, off: Int): (Int, Int) = {
-    fileReadIdx = idx
-    fileReadOff = off
-    (fileReadIdx, fileReadOff)
-  }
+    assert(fileList != Nil)
+    assert(idx < fileList.size && off < fileList(idx).length.toInt)
 
-  def seekWrite(idx: Int, off: Int): (Int, Int) = {
-    fileWriteIdx = idx
-    fileWriteOff = off
-    (fileWriteIdx, fileWriteOff)
+    fileIdx = idx
+    fileOff = off
+    (fileIdx, fileOff)
   }
 
   def readOneRecord(): Record = {
-    val stream = new RandomAccessFile(fileList(fileReadIdx), "r")
-    stream.seek(fileReadOff)
+    assert(fileList != Nil)
+
+    val stream = new RandomAccessFile(fileList(fileIdx), "r")
+    stream.seek(fileOff)
 
     val line = stream.readLine()
-    fileReadOff += line.size + 2 // gensort uses \r\n
-    if (fileReadOff >= stream.length()) {
-      fileReadOff -= stream.length().toInt
-      fileReadIdx += 1
+    fileOff += line.size + 2 // gensort uses \r\n
+    if (fileOff >= stream.length()) {
+      fileOff -= stream.length().toInt
+      fileIdx += 1
     }
     
     val rec = new Record(line.slice(0,10), line.slice(10,line.size))
@@ -55,18 +45,63 @@ class MultiFile(private[this] var fileList: List[File]) {
     rec
   }
 
-  def writeOneRecord(rec: Record): Unit = {
-
-  }
-
   def removeFiles(): Unit = {
+    assert(fileList != Nil)
+
     val deletedFiles = for {
       file <- fileList
       if (file.delete())
     } yield file
 
     assert(fileList.size == deletedFiles.size)
+  }
+}
 
-    fileList = List()
+class MultiFileWrite(private[this] val filePath: String) {
+  val logger = LoggerFactory.getLogger(getClass)
+
+  private[this] var fileIdx, fileOff = 0
+  private[this] var fileList: List[File] = List.empty
+
+  def seekWrite(idx: Int, off: Int): (Int, Int) = {
+    assert(idx < fileList.size && off < fileList(idx).length.toInt)
+
+    fileIdx = idx
+    fileOff = off
+    (fileIdx, fileOff)
+  }
+
+  def writeOneRecord(rec: Record): Unit = {
+    if (fileList.isEmpty) {
+      assert(fileIdx == 0 && fileOff == 0)
+
+      fileList = List(new File(filePath + "/" + MultiFileWrite.getFreshFileName()))
+    }
+
+    val stream = new RandomAccessFile(fileList(fileIdx), "rw")
+    stream.seek(fileOff)
+    stream.writeBytes(rec.key + rec.value)
+    fileOff += 100 // 100 = record size
+    if (fileOff >= 32*1000*1000) { // TODO: move this literal to config
+      assert(stream.length().toInt == 32*1000*1000)
+      
+      fileOff -= stream.length().toInt
+      fileIdx += 1
+      fileList = fileList :+ new File(filePath + "/" + MultiFileWrite.getFreshFileName())
+    }
+
+    stream.close()
+  }
+
+  def getFileList(): List[File] = fileList
+}
+
+object MultiFileWrite {
+  var fileNameCounter = 0
+
+  def getFreshFileName(): String = {
+    val fileName = "merging." + fileNameCounter.toString
+    fileNameCounter += 1
+    fileName
   }
 }
--- Worker/src/test/scala/MergeSuite.scala
+++ Worker/src/test/scala/MergeSuite.scala
@@ -2,7 +2,9 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.junit.runner.RunWith
 import org.scalatestplus.junit.JUnitRunner
 
+import java.security.MessageDigest
 import java.io.File
+import scala.io.Source
 
 @RunWith(classOf[JUnitRunner])
 class MergeSuite extends AnyFunSuite {
@@ -11,11 +13,17 @@ class MergeSuite extends AnyFunSuite {
 
 @RunWith(classOf[JUnitRunner])
 class MultiFileSuite extends AnyFunSuite {
-  val sampleFile1 = new File("Worker/src/test/files/sample")
-  val sampleFile2 = new File("Worker/src/test/files/sample2")
+  // https://stackoverflow.com/questions/38855843/scala-one-liner-to-generate-md5-hash-from-string
+  def md5sum(inputStr: String): String = {
+    val md: MessageDigest = MessageDigest.getInstance("MD5")
+    md.digest(inputStr.getBytes()).map(0xFF & _).map { "%02x".format(_) }.foldLeft("") {_ + _}
+  }
+
+  val sampleReadFile1 = new File("Worker/src/test/files/merge/sample")
+  val sampleReadFile2 = new File("Worker/src/test/files/merge/sample2")
 
   test("single file read test") {
-    val mf1 = new MultiFile(List(sampleFile1))
+    val mf1 = new MultiFileRead(List(sampleReadFile1))
     assert(mf1.readOneRecord.key == "AsfAGHM5om")
     assert(mf1.readOneRecord.key == "~sHd0jDv6X")
     assert(mf1.seekRead(0, 500) == (0,500))
@@ -23,9 +31,31 @@ class MultiFileSuite extends AnyFunSuite {
   }
 
   test("multiple file read test") {
-    val mf1 = new MultiFile(List(sampleFile1, sampleFile2))
+    val mf1 = new MultiFileRead(List(sampleReadFile1, sampleReadFile2))
     assert(mf1.seekRead(0, 900) == (0,900))
     assert(mf1.readOneRecord.key == "5HA\\z%qt{%")
     assert(mf1.readOneRecord.key == "`PkXQ<&+cc")
+  }
+
+  test("multiple file length test") {
+    val mf1 = new MultiFileRead(List(sampleReadFile1, sampleReadFile2))
+    assert(mf1.length == 2000)
+  }
+
+  test("record write test") {
+    val mf1 = new MultiFileWrite("Worker/src/test/files/merge")
+    mf1.writeOneRecord(new Record("ABCDEFGHIJ", "  00000000000000000000000000000000  0000222200002222000022220000222200002222000000001111\r\n"))
+    mf1.writeOneRecord(new Record("BCDEFGHIJK", "  00000000000000000000000000000000  0000222200002222000022220313222200002222000000001111\r\n"))
+    assert(md5sum(Source.fromFile(mf1.getFileList()(0).getAbsolutePath()).mkString) == "e12795094960457f6333520ec4f8dbc0")
+    mf1.getFileList()(0).delete()
+  }
+
+  test("write multiple files test") {
+    val mf1 = new MultiFileWrite("Worker/src/test/files/merge")
+    (1 to 400000).foreach(x => mf1.writeOneRecord(new Record("ABCDEFGHIJ", "  00000000000000000000000000000000  0000222200002222000022220000222200002222000000001111\r\n")))
+    assert(md5sum(Source.fromFile(mf1.getFileList()(0).getAbsolutePath()).mkString) == "ac7ec3cef9e3bd8e05400e0eb9040a32")
+    assert(md5sum(Source.fromFile(mf1.getFileList()(1).getAbsolutePath()).mkString) == "af53f02ccc505202fba4cdc612fe3707")
+    mf1.getFileList()(0).delete()
+    mf1.getFileList()(1).delete()
   }
 }
