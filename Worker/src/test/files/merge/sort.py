1c047cd951cd798f1a0f3331172966a875766cb8
candymate <ys3f4@naver.com>
2021-11-22 12:25:28 -0800

    merge: script for generating sorted test cases

---
+++ Worker/src/test/files/merge/sort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+
+raw = ""
+with open(sys.argv[1], "r") as f:
+  raw = f.read()
+
+raw = raw.split("\r\n")[:-1]
+raw.sort(key=lambda x: x[0:10])
+
+with open(sys.argv[1]+"_sorted", "w") as f:
+  f.write("\r\n".join(raw)+"\r\n")
